b82a9328e1f115fcd0db94d02adf99c8b06494b5
Dario <bahnhof2000@yahoo.it>
2016-12-17 15:17:07 +0100

    Aggiunta classe Listener e TemplateQueue

---
+++ ProgettoCS/TemplateQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace provaProgetto
+{
+    class TemplateQueue<T>
+    {
+        //Queue che funge da buffer per le operazioni da eseguire
+        //Il main (Program.cs) preleva, ListenerServer accoda
+        //Non ho capito come funzionano le ConcurrentQueue, spero siano
+        //  davvero thread safe se no va implementata la mutua esclusione
+        //  e sbatti vari che so bene come funzionano...già già
+        private ConcurrentQueue<T> queue;
+
+        public TemplateQueue()
+        {
+            queue = new ConcurrentQueue<T>();
+        }
+
+        //Sarà davvero thread safe?
+        //Scopri un modo per testare se è vero
+        public T getNextElement()
+        {
+            T element = default(T);
+            bool dequeued = queue.TryDequeue(out element);
+            if (dequeued)
+                return element;
+            else
+                return default(T);
+        }
+
+        //Sarà davvero thread safe?
+        public void enqueueElement(T el)
+        {
+            if (el != null)
+            {
+                queue.Enqueue(el);
+            }
+        }
+
+
+    }
+}
