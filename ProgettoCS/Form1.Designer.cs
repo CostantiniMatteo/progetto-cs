669e1c25155dda006d7ef3c129ea90ae40ce2179
Dario <bahnhof2000@yahoo.it>
2016-12-22 17:23:48 +0100

    Aggiunto mio form

--- ProgettoCS/Form1.Designer.cs
+++ ProgettoCS/Form1.Designer.cs
@@ -1,34 +1,146 @@
-﻿namespace ProgettoCS {
-    partial class Form1 {
+﻿namespace ProgettoCS
+{
+    partial class Form1
+    {
         /// <summary>
-        /// Required designer variable.
+        /// Variabile di progettazione necessaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Pulire le risorse in uso.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if(disposing && (components != null)) {
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Codice generato da Progettazione Windows Form
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.zedGraphControl3 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.zedGraphControl1.Location = new System.Drawing.Point(3, 3);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(575, 230);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(594, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(203, 673);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.zedGraphControl3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.zedGraphControl2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.zedGraphControl1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(581, 711);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // zedGraphControl3
+            // 
+            this.zedGraphControl3.BackColor = System.Drawing.SystemColors.Control;
+            this.zedGraphControl3.Location = new System.Drawing.Point(3, 240);
+            this.zedGraphControl3.Name = "zedGraphControl3";
+            this.zedGraphControl3.ScrollGrace = 0D;
+            this.zedGraphControl3.ScrollMaxX = 0D;
+            this.zedGraphControl3.ScrollMaxY = 0D;
+            this.zedGraphControl3.ScrollMaxY2 = 0D;
+            this.zedGraphControl3.ScrollMinX = 0D;
+            this.zedGraphControl3.ScrollMinY = 0D;
+            this.zedGraphControl3.ScrollMinY2 = 0D;
+            this.zedGraphControl3.Size = new System.Drawing.Size(575, 230);
+            this.zedGraphControl3.TabIndex = 3;
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.BackColor = System.Drawing.SystemColors.Control;
+            this.zedGraphControl2.Location = new System.Drawing.Point(3, 477);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(575, 231);
+            this.zedGraphControl2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(595, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(803, 735);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(18, 9, 18, 9);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private ZedGraph.ZedGraphControl zedGraphControl3;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ProgettoCS/Form1.cs
+++ ProgettoCS/Form1.cs
@@ -3,15 +3,208 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZedGraph;
 
-namespace ProgettoCS {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace ProgettoCS
+{
+    public partial class Form1 : Form
+    {
+
+        private Listener l1;
+        GraphPane myPane, myPane2, myPane3;
+        private TemplateQueue<string> stringQueue;
+        private TemplateQueue<List<List<double>>> valQueue;
+        static int jumpCount = 0;
+
+        public Form1()
+        {
             InitializeComponent();
+            myPane = zedGraphControl1.GraphPane;
+            myPane2 = zedGraphControl2.GraphPane;
+            myPane3 = zedGraphControl3.GraphPane;
+            myPane.Title.Text = "Accelerometro";
+            myPane2.Title.Text = "Giroscopio";
+            myPane3.Title.Text = "Magnetometro";
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            myPane2.XAxis.MajorGrid.IsVisible = true;
+            myPane2.YAxis.MajorGrid.IsVisible = true;
+            myPane3.XAxis.MajorGrid.IsVisible = true;
+            myPane3.YAxis.MajorGrid.IsVisible = true;
+
+            //myPane.Chart.Fill.Brush = new System.Drawing.SolidBrush(Color.DimGray);
+            //Per settare valori massimi e minimi del grafico
+            /*myPane.YAxis.Scale.Min = -1;
+            myPane.YAxis.Scale.Max = 1;
+            myPane.XAxis.Scale.Min = -1;
+            myPane.XAxis.Scale.Max = 1;
+            myPane.AxisChange();
+            zedGraphControl1.Invalidate();*/
+            stringQueue = new TemplateQueue<string>();
+            valQueue = new TemplateQueue<List<List<double>>>();
+
+            l1 = new Listener(valQueue, stringQueue);
+
+            Thread t1 = new Thread(drawModule);
+            Thread t2 = new Thread(l1.parser);
+            Thread t3 = new Thread(stampa);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+        public void draw()
+        {
+            List<List<double>> val;
+            PointPairList pointPl = new PointPairList();
+            PointPairList pointPl2 = new PointPairList();
+            PointPairList pointPl3 = new PointPairList();
+
+            while (true)
+            {
+                val = valQueue.getNextElement();
+                if (val != null)
+                {
+                    int size = val.Count();
+                    for (int i = 1; i < size; i++)
+                    {
+                        pointPl.Add(val[i][0], val[i][1]);
+                        pointPl2.Add(val[i][3], val[i][4]);
+                        pointPl3.Add(val[i][6], val[i][7]);
+                    }
+                    zedGraphControl1.GraphPane.CurveList.Clear();
+                    zedGraphControl2.GraphPane.CurveList.Clear();
+                    zedGraphControl3.GraphPane.CurveList.Clear();
+                    LineItem myCurve = myPane.AddCurve("", pointPl, Color.Red, SymbolType.None);
+                    LineItem myCurve2 = myPane2.AddCurve("", pointPl2, Color.Blue, SymbolType.None);
+                    LineItem myCurve3 = myPane3.AddCurve("", pointPl3, Color.Green, SymbolType.None);
+
+                    zedGraphControl1.AxisChange();
+                    zedGraphControl1.Invalidate();
+
+                    zedGraphControl2.AxisChange();
+                    zedGraphControl2.Invalidate();
+
+                    zedGraphControl3.AxisChange();
+                    zedGraphControl3.Invalidate();
+
+                    //zedGraphControl1.Refresh();
+                }
+            }
+
+        }
+
+        public void drawModule()
+        {
+            List<List<double>> val;
+            PointPairList pointPl = new PointPairList();
+            PointPairList pointPl2 = new PointPairList();
+            PointPairList pointPl3 = new PointPairList();
+            List<double> modacc = new List<double>();
+            List<double> modgyr = new List<double>();
+            double x = 0;
+
+            while (true)
+            {
+                val = valQueue.getNextElement();
+                if (val != null)
+                {
+
+                    modacc.Add(modulo(val[0][0], val[0][1], val[0][2]));
+                    modgyr.Add(modulo(val[0][3], val[0][4], val[0][5]));
+
+                    pointPl.Add(x, modacc[modacc.Count - 1]);
+                    pointPl2.Add(x, modgyr[modgyr.Count - 1]);
+
+                    double y = Math.Atan(val[0][7] / val[0][8]);
+                    y = deleteDiscontinuity(pointPl3, y, x);
+
+
+                    pointPl3.Add(x, y);
+                    x++;
+                    zedGraphControl1.GraphPane.CurveList.Clear();
+                    zedGraphControl2.GraphPane.CurveList.Clear();
+                    zedGraphControl3.GraphPane.CurveList.Clear();
+
+                    LineItem myCurve = myPane.AddCurve("", pointPl, Color.Red, SymbolType.None);
+                    LineItem myCurve2 = myPane2.AddCurve("", pointPl2, Color.Blue, SymbolType.None);
+                    LineItem myCurve3 = myPane3.AddCurve("Theta", pointPl3, Color.Green, SymbolType.None);
+
+                    zedGraphControl1.AxisChange();
+                    zedGraphControl1.Invalidate();
+
+                    zedGraphControl2.AxisChange();
+                    zedGraphControl2.Invalidate();
+
+                    zedGraphControl3.AxisChange();
+                    zedGraphControl3.Invalidate();
+
+                    //zedGraphControl1.Refresh();
+                }
+            }
+        }
+
+        private double incremRapp(double y1, double x1, double y2, double x2)
+        {
+            return (y2 - y1) / (x2 - x1);
+        }
+
+        private double deleteDiscontinuity(PointPairList pointPl3, double y, double x)
+        {
+            double oldY, oldX;
+            double incrRapp;
+            if (pointPl3.Count > 0 && pointPl3[pointPl3.Count - 1] != null)
+            {
+                oldX = pointPl3[pointPl3.Count - 1].X;
+                oldY = pointPl3[pointPl3.Count - 1].Y;
+
+                incrRapp = incremRapp(oldY, oldX, y, x);
+
+                if(incrRapp > 3)
+                {
+                    y = y - Math.PI;
+
+                }else if (incrRapp < -3)
+                {
+                    y = y + Math.PI;
+                }
+            }
+            return y;
+        }
+
+        private double modulo(double v1, double v2, double v3)
+        {
+            return Math.Sqrt(Math.Abs(v1) + Math.Abs(v2) + Math.Abs(v3));
+        }
+
+
+
+        public void stampa()
+        {
+            string stringa;
+            while (true)
+            {
+                stringa = stringQueue.getNextElement();
+
+                if (stringa != null)
+                    try
+                    {
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+                            richTextBox1.Text += stringa;
+                        });
+                    }
+                    catch (Exception e) { }
+                Thread.Sleep(100);
+            }
         }
     }
 }
